mizdrake7/WILDFlower-android13-5.15-common-GKI

commit 857c6e0616d4678ab5cb9415cee5957ec043de7c

ANDROID: Track per-process dmabuf RSS
DMA buffers exist for sharing memory (between processes, drivers, and
hardware) so they are not accounted the same way as user memory present
on a MM's LRUs. Per-process attribution of dmabuf memory is not
maintained by the kernel, so to obtain it from userspace, several files
from procfs and sysfs must be read any time the information is desired.
This process is slow, which can lead to dmabuf accounting information
being out-of-date when it is desired during events like low memory, or
bugreport generation, masking the cause of memory issues.

This patch attributes dmabuf memory to any process that holds a
reference to a buffer. A process can hold a reference to a dmabuf in two
ways:
  1) Through a file descriptor
  2) Though a mapping

A single buffer can be referenced more than once by a single process
with multiple file descriptors for the same buffer, multiple mappings
for the same buffer, or any combination of the two.

The full size of a buffer is effectively pinned until no references
exist from any process, or anywhere else in the kernel such as drivers
that have imported the buffer. Even if a partial mapping of the buffer
is the only reference that exists. Therefore buffer accounting is always
performed in units of the full buffer size, and only once for each
process, regardless of the number and type of references a process has
for a single buffer.

The /proc/<pid>/dmabuf_rss file in procfs now reports the sum of all
buffer sizes referenced by a process. The units are bytes. This allows
userspace to obtain per-process dmabuf accounting information quickly
compared to calculating it from multiple sources in procfs and sysfs.

Note that a dmabuf can be backed by different types of memory such as
system DRAM, GPU VRAM, or others. This patch makes no distinction
between these different types of memory, so on systems with non-unified
memory the reported values should be interpreted with this in mind.

Bug: 424648392

drivers/dma-buf/dma-buf.c
@@ -357,9 +357,267 @@ static void trim_task_dmabuf_records(void)
 	local_unlock(&dmabuf_rec_reloads.lock);
 }
 
+static struct task_dma_buf_record *find_task_dmabuf_record(
+		struct task_dma_buf_info *dmabuf_info, struct dma_buf *dmabuf)
+{
+	struct task_dma_buf_record *rec;
+
+	lockdep_assert_held(&dmabuf_info->lock);
+
+	list_for_each_entry(rec, &dmabuf_info->dmabufs, node)
+		if (dmabuf == rec->dmabuf)
+			return rec;
+
+	return NULL;
+}
+
+static void add_task_dmabuf_record(struct task_dma_buf_info *dmabuf_info,
+				   struct dma_buf *dmabuf,
+				   struct task_dma_buf_record *rec)
+{
+	lockdep_assert_held(&dmabuf_info->lock);
+
+	rec->dmabuf = dmabuf;
+	rec->refcnt = 1;
+	list_add(&rec->node, &dmabuf_info->dmabufs);
+	dmabuf_info->dmabuf_count++;
+	dmabuf_info->rss += dmabuf->size;
+}
+
+/**
+ * dma_buf_account_task - Account a dmabuf to a task
+ * @dmabuf:	[in]	pointer to dma_buf
+ * @task:	[in]	pointer to task_struct
+ *
+ * When a process obtains a dmabuf file descriptor, or maps a dmabuf, this
+ * function attributes the provided @dmabuf to the @task. The first time @dmabuf
+ * is attributed to @task, the buffer's size is added to the @task's dmabuf RSS.
+ *
+ * Return:
+ * * 0 on success
+ * * A negative error code upon error
+ */
+int dma_buf_account_task(struct dma_buf *dmabuf, struct task_struct *task)
+{
+	struct task_dma_buf_info *dmabuf_info = task->dmabuf_info;
+	struct task_dma_buf_record *rec;
+
+	if (!dmabuf_info)
+		return 0;
+
+	if (!task_dmabuf_records_preload(1))
+		return -ENOMEM;
+
+	spin_lock(&dmabuf_info->lock);
+	rec = find_task_dmabuf_record(dmabuf_info, dmabuf);
+	if (rec) {
+		++rec->refcnt;
+		trim_task_dmabuf_records_locked();
+	} else {
+		rec = alloc_task_dmabuf_record();
+		WARN_ON(!rec);
+		add_task_dmabuf_record(dmabuf_info, dmabuf, rec);
+	}
+	spin_unlock(&dmabuf_info->lock);
+	task_dmabuf_records_preload_end();
+
+	return 0;
+}
+
+/**
+ * dma_buf_unaccount_task - Unaccount a dmabuf from a task
+ * @dmabuf:	[in]	pointer to dma_buf
+ * @task:	[in]	pointer to task_struct
+ *
+ * When a process closes a dmabuf file descriptor, or unmaps a dmabuf, this
+ * function removes the provided @dmabuf attribution from the @task. When all
+ * references to @dmabuf are removed from @task, the buffer's size is removed
+ * from the task's dmabuf RSS.
+ */
+void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task)
+{
+	struct task_dma_buf_info *dmabuf_info = task->dmabuf_info;
+	struct task_dma_buf_record *rec;
+
+	if (!dmabuf_info)
+		return;
+
+	spin_lock(&dmabuf_info->lock);
+	rec = find_task_dmabuf_record(dmabuf_info, dmabuf);
+	if (rec) {
+		if (--rec->refcnt == 0) {
+			list_del(&rec->node);
+			dmabuf_info->dmabuf_count--;
+			dmabuf_info->rss -= dmabuf->size;
+		} else {
+			rec = NULL;
+		}
+	} else {
+		pr_err("Could not find dmabuf %lu in unaccount for task %d\n",
+		       file_inode(dmabuf->file)->i_ino, task_pid_nr(task));
+	}
+	spin_unlock(&dmabuf_info->lock);
+	if (rec)
+		free_task_dmabuf_record(rec);
+}
+
+static struct task_dma_buf_info *alloc_task_dma_buf_info(void)
+{
+	struct task_dma_buf_info *dmabuf_info;
+
+	dmabuf_info = kzalloc(sizeof(*dmabuf_info), GFP_KERNEL);
+	if (!dmabuf_info)
+		return NULL;
+
+	refcount_set(&dmabuf_info->refcnt, 1);
+	spin_lock_init(&dmabuf_info->lock);
+	INIT_LIST_HEAD(&dmabuf_info->dmabufs);
+
+	return dmabuf_info;
+}
+
+static struct task_dma_buf_info *dup_dma_buf_info(struct task_dma_buf_info *from)
+{
+	struct task_dma_buf_info *to;
+	struct task_dma_buf_record *from_rec, *to_rec;
+	unsigned int count;
+	int retries = 0;
+
+	/* Allocate now before locked section below. */
+	to = alloc_task_dma_buf_info();
+	if (!to)
+		return NULL;
+
+	/* Read required count racily, before obtaining dmabuf_info->lock */
+	count = READ_ONCE(from->dmabuf_count);
+	if (!task_dmabuf_records_preload(count))
+		goto err_list_copy;
+
+retry:
+	spin_lock(&from->lock);
+	if (from->dmabuf_count > count) {
+		/* We don't have enough reserved records, allocate more */
+		count = from->dmabuf_count;
+
+		spin_unlock(&from->lock);
+		task_dmabuf_records_preload_end();
+		if (!task_dmabuf_records_preload(count))
+			goto err_list_copy;
+
+		/* Limit the number of retries to avoid live-lock */
+		if (retries++ > 5) {
+			task_dmabuf_records_preload_end();
+			goto err_list_copy;
+		}
+
+		goto retry;
+	}
+
+	/* All required records are reserved */
+	list_for_each_entry(from_rec, &from->dmabufs, node) {
+		to_rec = alloc_task_dmabuf_record();
+		WARN_ON(!to_rec);
+		to_rec->dmabuf = from_rec->dmabuf;
+		to_rec->refcnt = from_rec->refcnt;
+		list_add(&to_rec->node, &to->dmabufs);
+	}
+	to->dmabuf_count = from->dmabuf_count;
+	to->rss = from->rss;
+	spin_unlock(&from->lock);
+
+	trim_task_dmabuf_records_locked();
+	task_dmabuf_records_preload_end();
+
+	return to;
+
+err_list_copy:
+	trim_task_dmabuf_records();
+	kfree(to);
+
+	return NULL;
+}
+
+int copy_dmabuf_info(u64 clone_flags, struct task_struct *task)
+{
+	struct task_dma_buf_info *parent_dmabuf_info = current->dmabuf_info;
+	struct task_dma_buf_info *child_dmabuf_info;
+	bool share_vm = clone_flags & CLONE_VM;
+	bool share_fs = clone_flags & CLONE_FILES;
+
+	/* kthreads are not supported */
+	if (task->flags & PF_KTHREAD) {
+		task->dmabuf_info = NULL;
+		return 0;
+	}
+
+	/*
+	 * Non-kthread direct descendants of pid 0 are roots of their own task_dma_buf_info trees,
+	 * even if they want to partially share with pid 0. Init does this. We assume no dmabuf
+	 * sharing will actually occur through pid 0.
+	 */
+	if (unlikely(!task_pid_nr(current))) {
+		task->dmabuf_info = alloc_task_dma_buf_info();
+		if (!task->dmabuf_info)
+			return -ENOMEM;
+
+		return 0;
+	}
+
+	/*
+	 * Partial sharing is not supported.
+	 * Children of such tasks are also not supported.
+	 */
+	if (share_vm != share_fs || !parent_dmabuf_info) {
+		task->dmabuf_info = NULL;
+		return 0;
+	}
+
+	/*
+	 * Full sharing: Both MM and FD references to dmabufs are shared with
+	 * the parent, so they can both share the same dmabuf_info.
+	 */
+	if (share_vm && share_fs) {
+		refcount_inc(&parent_dmabuf_info->refcnt);
+		task->dmabuf_info = parent_dmabuf_info;
+		return 0;
+	}
+
+	/*
+	 * No sharing: Both MM and FD references to dmabufs are duplicated in the child. We
+	 * duplicate the dmabuf accounting info into the child as well here.
+	 */
+	child_dmabuf_info = dup_dma_buf_info(parent_dmabuf_info);
+	if (!child_dmabuf_info)
+		return -ENOMEM;
+
+	task->dmabuf_info = child_dmabuf_info;
+
+	return 0;
+}
+
+void put_dmabuf_info(struct task_struct *task)
+{
+	if (!task->dmabuf_info)
+		return;
+
+	if (!refcount_dec_and_test(&task->dmabuf_info->refcnt))
+		return;
+
+	if (task->dmabuf_info->rss)
+		pr_alert("destroying task with non-zero dmabuf rss %lu\n", task->dmabuf_info->rss);
+
+	if (!list_empty(&task->dmabuf_info->dmabufs) || task->dmabuf_info->dmabuf_count > 0)
+		pr_alert("destroying task with non-empty dmabuf list %zu %u\n",
+			 list_count_nodes(&task->dmabuf_info->dmabufs),
+			 task->dmabuf_info->dmabuf_count);
+
+	kfree(task->dmabuf_info);
+}
+
 static int dma_buf_mmap_internal(struct file *file, struct vm_area_struct *vma)
 {
 	struct dma_buf *dmabuf;
+	int ret;
 
 	if (!is_dma_buf_file(file))
 		return -EINVAL;
@@ -375,7 +633,15 @@ static int dma_buf_mmap_internal(struct file *file, struct vm_area_struct *vma)
 	    dmabuf->size >> PAGE_SHIFT)
 		return -EINVAL;
 
-	return dmabuf->ops->mmap(dmabuf, vma);
+	ret = dmabuf->ops->mmap(dmabuf, vma);
+	if (!ret) {
+		int err = dma_buf_account_task(dmabuf, current);
+
+		if (err)
+			pr_err("dmabuf accounting failed during mmap operation, err %d\n", err);
+	}
+
+	return ret;
 }
 
 static loff_t dma_buf_llseek(struct file *file, loff_t offset, int whence)
@@ -752,6 +1018,13 @@ static void dma_buf_show_fdinfo(struct seq_file *m, struct file *file)
 	spin_unlock(&dmabuf->name_lock);
 }
 
+static int dma_buf_flush(struct file *file, fl_owner_t id)
+{
+	/* When dmabuf FD is closed we should unaccount it */
+	dma_buf_unaccount_task(file->private_data, current);
+	return 0;
+}
+
 static const struct file_operations dma_buf_fops = {
 	.release	= dma_buf_file_release,
 	.mmap		= dma_buf_mmap_internal,
@@ -760,6 +1033,7 @@ static const struct file_operations dma_buf_fops = {
 	.unlocked_ioctl	= dma_buf_ioctl,
 	.compat_ioctl	= compat_ptr_ioctl,
 	.show_fdinfo	= dma_buf_show_fdinfo,
+	.flush		= dma_buf_flush,
 };
 
 /*
@@ -1748,6 +2022,8 @@ EXPORT_SYMBOL_GPL(dma_buf_end_cpu_access_partial);
 int dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma,
 		 unsigned long pgoff)
 {
+	int ret;
+
 	if (WARN_ON(!dmabuf || !vma))
 		return -EINVAL;
 
@@ -1768,7 +2044,15 @@ int dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma,
 	vma_set_file(vma, dmabuf->file);
 	vma->vm_pgoff = pgoff;
 
-	return dmabuf->ops->mmap(dmabuf, vma);
+	ret = dmabuf->ops->mmap(dmabuf, vma);
+	if (!ret) {
+		int err = dma_buf_account_task(dmabuf, current);
+
+		if (err)
+			pr_err("dmabuf accounting failed during mmap operation, err %d\n", err);
+	}
+
+	return ret;
 }
 EXPORT_SYMBOL_NS_GPL(dma_buf_mmap, DMA_BUF);
 
@@ -2022,6 +2306,7 @@ static int __init dma_buf_init(void)
 	if (IS_ERR(dma_buf_mnt))
 		return PTR_ERR(dma_buf_mnt);
 
+	init_task_dmabuf_record_pool();
 	dma_buf_init_debugfs();
 	return 0;
 }

fs/file.c
@@ -20,6 +20,7 @@
 #include <linux/spinlock.h>
 #include <linux/rcupdate.h>
 #include <linux/close_range.h>
+#include <linux/dma-buf.h>
 #include <net/sock.h>
 
 #include "internal.h"
@@ -592,6 +593,14 @@ void fd_install(unsigned int fd, struct file *file)
 {
 	struct files_struct *files = current->files;
 	struct fdtable *fdt;
+
+	if (is_dma_buf_file(file)) {
+		int err = dma_buf_account_task(file->private_data, current);
+
+		if (err)
+			pr_err("dmabuf accounting failed during fd_install operation, err %d\n",
+			       err);
+	}
 
 	rcu_read_lock_sched();
 

fs/proc/base.c
@@ -100,6 +100,7 @@
 #include <linux/cn_proc.h>
 #include <linux/ksm.h>
 #include <linux/cpufreq_times.h>
+#include <linux/dma-buf.h>
 #include <trace/events/oom.h>
 #include <trace/hooks/sched.h>
 #include "internal.h"
@@ -3304,6 +3305,25 @@ static int proc_stack_depth(struct seq_file *m, struct pid_namespace *ns,
 }
 #endif /* CONFIG_STACKLEAK_METRICS */
 
+#ifdef CONFIG_DMA_SHARED_BUFFER
+static int proc_dmabuf_rss_show(struct seq_file *m, struct pid_namespace *ns,
+		     struct pid *pid, struct task_struct *task)
+{
+	struct task_dma_buf_info *dmabuf_info = task->dmabuf_info;
+
+	if (dmabuf_info) {
+		unsigned long rss;
+
+		spin_lock(&dmabuf_info->lock);
+		rss = dmabuf_info->rss;
+		spin_unlock(&dmabuf_info->lock);
+		seq_printf(m, "%lu\n", rss);
+	}
+
+	return 0;
+}
+#endif
+
 /*
  * Thread groups
  */
@@ -3426,6 +3446,9 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_KSM
 	ONE("ksm_merging_pages",  S_IRUSR, proc_pid_ksm_merging_pages),
 	ONE("ksm_stat",  S_IRUSR, proc_pid_ksm_stat),
+#endif
+#ifdef CONFIG_DMA_SHARED_BUFFER
+	ONE("dmabuf_rss", 0444, proc_dmabuf_rss_show),
 #endif
 };
 

include/linux/dma-buf.h
@@ -24,6 +24,7 @@
 #include <linux/wait.h>
 #include <linux/workqueue.h>
 #include <linux/android_kabi.h>
+#include <linux/refcount.h>
 
 struct device;
 struct dma_buf;
@@ -656,6 +657,29 @@ struct task_dma_buf_record {
 };
 
 /**
+ * struct task_dma_buf_info - Holds a RSS counter, and a list of dmabufs for all
+ * tasks that share both mm_struct and files_struct.
+ *
+ * @rss: The sum of all dmabuf memory referenced by the task(s) via memory
+ *       mappings or file descriptors in bytes. Buffers referenced more than
+ *       once by the process (multiple mmaps, multiple FDs, or any combination
+ *       of both mmaps and FDs) only cause the buffer to be accounted to the
+ *       process once. Partial mappings cause the full size of the buffer to be
+ *       accounted, regardless of the size of the mapping.
+ * @refcnt: The number of tasks sharing this struct.
+ * @lock: Lock protecting @rss, @dmabufs, and @dmabuf_count.
+ * @dmabufs: List of all dmabufs referenced by the task(s).
+ * @dmabuf_count: The number of elements on the @dmabufs list.
+ */
+struct task_dma_buf_info {
+	unsigned long rss;
+	refcount_t refcnt;
+	spinlock_t lock;
+	struct list_head dmabufs;
+	unsigned int dmabuf_count;
+};
+
+/**
  * DEFINE_DMA_BUF_EXPORT_INFO - helper macro for exporters
  * @name: export-info name
  *
@@ -707,7 +731,6 @@ dma_buf_attachment_is_dynamic(struct dma_buf_attachment *attach)
 	return !!attach->importer_ops;
 }
 
-int is_dma_buf_file(struct file *file);
 int dma_buf_get_each(int (*callback)(const struct dma_buf *dmabuf,
 		     void *private), void *private);
 struct dma_buf_attachment *dma_buf_attach(struct dma_buf *dmabuf,
@@ -759,4 +782,26 @@ long dma_buf_set_name(struct dma_buf *dmabuf, const char *name);
 int dma_buf_get_flags(struct dma_buf *dmabuf, unsigned long *flags);
 struct dma_buf *dma_buf_iter_begin(void);
 struct dma_buf *dma_buf_iter_next(struct dma_buf *dmbuf);
+
+#ifdef CONFIG_DMA_SHARED_BUFFER
+
+int is_dma_buf_file(struct file *file);
+int dma_buf_account_task(struct dma_buf *dmabuf, struct task_struct *task);
+void dma_buf_unaccount_task(struct dma_buf *dmabuf, struct task_struct *task);
+int copy_dmabuf_info(u64 clone_flags, struct task_struct *task);
+void put_dmabuf_info(struct task_struct *task);
+
+#else /* CONFIG_DMA_SHARED_BUFFER */
+
+static inline int is_dma_buf_file(struct file *file) { return 0; }
+static inline int dma_buf_account_task(struct dma_buf *dmabuf,
+				       struct task_struct *task) { return 0; }
+static inline void dma_buf_unaccount_task(struct dma_buf *dmabuf,
+					  struct task_struct *task) {}
+static inline int copy_dmabuf_info(u64 clone_flags,
+				   struct task_struct *task) { return 0; }
+static inline void put_dmabuf_info(struct task_struct *task) {}
+
+#endif /* CONFIG_DMA_SHARED_BUFFER */
+
 #endif /* __DMA_BUF_H__ */

include/linux/sched.h
@@ -70,6 +70,7 @@ struct seq_file;
 struct sighand_struct;
 struct signal_struct;
 struct task_delay_info;
+struct task_dma_buf_info;
 struct task_group;
 struct user_event_mm;
 
@@ -1516,6 +1517,9 @@ struct task_struct {
 	 */
 	struct callback_head		l1d_flush_kill;
 #endif
+
+	struct task_dma_buf_info *dmabuf_info;
+
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);

init/init_task.c
@@ -214,6 +214,7 @@ struct task_struct init_task
 	.android_vendor_data1 = {0, },
 	.android_oem_data1 = {0, },
 #endif
+	.dmabuf_info = NULL,
 };
 EXPORT_SYMBOL(init_task);
 

kernel/fork.c
@@ -101,6 +101,7 @@
 #include <linux/iommu.h>
 #include <linux/tick.h>
 #include <linux/cpufreq_times.h>
+#include <linux/dma-buf.h>
 
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -1000,6 +1001,7 @@ void __put_task_struct(struct task_struct *tsk)
 	WARN_ON(refcount_read(&tsk->usage));
 	WARN_ON(tsk == current);
 
+	put_dmabuf_info(tsk);
 	io_uring_free(tsk);
 	cgroup_free(tsk);
 	task_numa_free(tsk, true);
@@ -1195,6 +1197,7 @@ static struct task_struct *dup_task_struct(struct task_struct *orig, int node)
 	tsk->task_frag.page = NULL;
 	tsk->wake_q.next = NULL;
 	tsk->worker_private = NULL;
+	tsk->dmabuf_info = NULL;
 
 	kcov_task_init(tsk);
 	kmsan_task_create(tsk);
@@ -2629,6 +2632,12 @@ __latent_entropy struct task_struct *copy_process(
 	p->rethooks.first = NULL;
 #endif
 
+	retval = copy_dmabuf_info(clone_flags, p);
+	if (retval) {
+		pr_err("failed to copy dmabuf accounting info, err %d\n", retval);
+		goto bad_fork_put_pidfd;
+	}
+
 	/*
 	 * Ensure that the cgroup subsystem policies allow the new process to be
 	 * forked. It should be noted that the new process's css_set can be changed
@@ -2637,7 +2646,7 @@ __latent_entropy struct task_struct *copy_process(
 	 */
 	retval = cgroup_can_fork(p, args);
 	if (retval)
-		goto bad_fork_put_pidfd;
+		goto bad_fork_cleanup_dmabuf;
 
 	/*
 	 * Now that the cgroups are pinned, re-clone the parent cgroup and put
@@ -2782,6 +2791,8 @@ bad_fork_cancel_cgroup:
 	spin_unlock(&current->sighand->siglock);
 	write_unlock_irq(&tasklist_lock);
 	cgroup_cancel_fork(p, args);
+bad_fork_cleanup_dmabuf:
+	put_dmabuf_info(p);
 bad_fork_put_pidfd:
 	if (clone_flags & CLONE_PIDFD) {
 		fput(pidfile);

mm/mmap.c
@@ -49,6 +49,7 @@
 #include <linux/oom.h>
 #include <linux/sched/mm.h>
 #include <linux/ksm.h>
+#include <linux/dma-buf.h>
 
 #include <linux/uaccess.h>
 #include <asm/cacheflush.h>
@@ -144,8 +145,11 @@ static void remove_vma(struct vm_area_struct *vma, bool unreachable)
 {
 	might_sleep();
 	vma_close(vma);
-	if (vma->vm_file)
+	if (vma->vm_file) {
+		if (is_dma_buf_file(vma->vm_file))
+			dma_buf_unaccount_task(vma->vm_file->private_data, current);
 		fput(vma->vm_file);
+	}
 	mpol_put(vma_policy(vma));
 	if (unreachable)
 		__vm_area_free(vma);
@@ -2417,8 +2421,15 @@ int __split_vma(struct vma_iterator *vmi, struct vm_area_struct *vma,
 	if (err)
 		goto out_free_mpol;
 
-	if (new->vm_file)
+	if (new->vm_file) {
 		get_file(new->vm_file);
+		if (is_dma_buf_file(new->vm_file)) {
+			int acct_err = dma_buf_account_task(new->vm_file->private_data, current);
+
+			if (acct_err)
+				pr_err("failed to account dmabuf, err %d\n", acct_err);
+		}
+	}
 
 	if (new->vm_ops && new->vm_ops->open)
 		new->vm_ops->open(new);

Zithxnm/gki-clo

commit 9eecad532ad396423fac2f19a772c836220c0806

ANDROID: dma-buf: Prevent VMA merging with VM_DONTEXPAND
The kernel's vma_merge logic prevents merging for any VMA with a
VM_SPECIAL flag (VM_IO | VM_DONTEXPAND | VM_PFNMAP | VM_MIXEDMAP). While
in-tree exporters usually apply one of these flags, out-of-tree vendor
heaps or exporters may mmap without them.

If a dmabuf VMA is allowed to merge, one of the VMAs will be destroyed
without calling remove_vma() where unaccounting is performed for
per-process dmabuf accounting. This results in a refcounting error that
can lead to a UAF.

Fix this by auditing the VMA flags after the exporter's mmap op returns
successfully. If the exporter failed to set a VM_SPECIAL flag, emit a
warning and forcefully set VM_DONTEXPAND to prevent VMA merges.

Bug: 508702426
Fixes: 857c6e0616d4 ("ANDROID: Track per-process dmabuf RSS")
Change-Id: I217f437f50ee509611fd4d8d25ecf4af7ac76a43
Signed-off-by: T.J. Mercier <tjmercier@google.com>

drivers/dma-buf/dma-buf.c
@@ -835,6 +835,13 @@ static int dma_buf_mmap_internal(struct file *file, struct vm_area_struct *vma)
 
 	ret = dmabuf->ops->mmap(dmabuf, vma);
 	if (!ret && vma->vm_file == file) {
+		/*
+		 * dmabuf VMAs must not be mergeable. If the exporter forgot to set a VM_SPECIAL
+		 * flag, force one now.
+		 */
+		if (WARN_ON(!(vma->vm_flags & VM_SPECIAL)))
+			vm_flags_set(vma, VM_DONTEXPAND);
+
 		int err = dma_buf_account_task(dmabuf, vma->vm_mm->dmabuf_info);
 
 		if (err)
@@ -2239,6 +2246,13 @@ int dma_buf_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma,
 
 	ret = dmabuf->ops->mmap(dmabuf, vma);
 	if (!ret && vma->vm_file == dmabuf->file) {
+		/*
+		 * dmabuf VMAs must not be mergeable. If the exporter forgot to set a VM_SPECIAL
+		 * flag, force one now.
+		 */
+		if (WARN_ON(!(vma->vm_flags & VM_SPECIAL)))
+			vm_flags_set(vma, VM_DONTEXPAND);
+
 		int err = dma_buf_account_task(dmabuf, vma->vm_mm->dmabuf_info);
 
 		if (err)